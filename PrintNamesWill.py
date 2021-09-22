e2f2d64fcf0670a7d384f3323615bc349d226815
William Chen <58865624+Iamchwill@users.noreply.github.com>
2021-09-22 08:37:54 -0400

    Updated Code For Invalid Inputs

--- PrintNamesWill.py
+++ PrintNamesWill.py
@@ -5,8 +5,11 @@ pd2 = ['studentA', 'studentB', 'studentC', 'studentD']
 import random
 
 def printName():
-    period = int(input('Pick a period (Input 1 or 2): '))
-    if period == 1:
+    period = input('Pick a period (Input 1 or 2): ')
+    if period == '1':
         print(pd1[random.randint(0,len(pd1) - 1)])
-    if period == 2:
+    elif period == '2':
         print(pd2[random.randint(0,len(pd2) - 1)])
+    else:
+        print('NOT AN OPTION')
+        printName()
