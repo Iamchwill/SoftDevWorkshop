315105e4d4d7a22e0e7ca41e93ce2d998bc1d9c8
iamchwill <wchen20@stuy.edu>
2021-09-29 21:19:09 -0400

    Edited code so it now works with the actual csv data

--- 06_py-csv/parseList.py
+++ 06_py-csv/parseList.py
@@ -8,23 +8,21 @@
 # Afterwards the probablity would be the percentage of the previous value + it's own.
 # I found random.choice() and talked about it.
 
+# V2
+# We realized we used the wrong text. Copy and pasted from Raw this time.
+# We used csv.Reader this time since the regular reader seperates commas even in quotes
+
+#imports
 import random
+import csv
 
-def selectDestiny():
-    #read the file and split at new line
-    file = open("occ_per.csv","r")
-    data = file.read()
-    dataList = data.split("\n")
-    
-    #put files into list
-    destinies = {}
-    #First line is title, last 2 are Total and empty
-    for x in range(1,len(dataList) - 2):
-        item = dataList[x].split("\t")
-        destinies[item[0]] = float(item[1])
-
-    jobs = list(destinies.keys())
-    percentages = list(destinies.values())
-    #choices() chooses k of the jobs randomly based on weighted list.
-    job = random.choices(jobs, percentages, k = 1);
+import random
+def selectJob():
+    dictionary ={}
+    with open('occ_per.csv', newline='') as csvfile:
+         reader = csv.DictReader(csvfile)
+         for row in reader:
+             if (row['Job Class']!= 'Total' and row['Job Class']!= 'Job Class'):
+                 dictionary[row['Job Class']] = float(row['Percentage'])
+    job = random.choices(list(dictionary.keys()),list(dictionary.values()), k = 1)
     return job[0]
