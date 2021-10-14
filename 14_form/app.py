ac759f1940d168ccb37bc05025be10b6bec66d86
iamchwill <wchen20@stuy.edu>
2021-10-14 18:27:12 -0400

    Added working (incomplete) 14_form

---
+++ 14_form/app.py
@@ -0,0 +1,27 @@
+# Clyde 'Thluffy' Sinclair
+# SoftDev
+# Oct 2021 
+
+from flask import Flask             #facilitate flask webserving
+from flask import render_template   #facilitate jinja templating
+from flask import request           #facilitate form submission
+
+#the conventional way:
+#from flask import Flask, render_template, request
+
+app = Flask(__name__)    #create Flask object
+
+@app.route("/") #, methods=['GET', 'POST'])
+def disp_loginpage():
+    return render_template( 'login.html' )
+
+
+@app.route("/auth") # , methods=['GET', 'POST'])
+def authenticate():
+    return render_template( 'response.html', username = request.args["username"], method = request.method)
+
+    
+if __name__ == "__main__": #false if this file imported as module
+    #enable debugging, auto-restarting of server when this file is modified
+    app.debug = True 
+    app.run()
