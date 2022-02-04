cacd3a718e74328108c2cfeb7daa08846701e9fd
Iamchwill <wchen20@stuy.edu>
2022-02-04 09:27:39 -0500

    JS test

---
+++ 27_js/fibfac.js
@@ -0,0 +1,21 @@
+// Team Zemmi :: Clyde Sinclair, Fierce Dragon 
+// SoftDev pd0
+// K27 -- Basic functions in JavaScript
+// 2022-02-03r
+// --------------------------------------------------
+
+
+// as a duo...
+// pair programming style,
+// implement a fact and fib fxn
+var fact = function(n) {
+if(n > 0){
+   n = (n * fact(n - 1));
+   }
+   return x;
+};
+
+var fxnName = function(args) {
+};
+
+//Do whatever you think is needed. Think: S I M P L E.   Think: S M A R T.
