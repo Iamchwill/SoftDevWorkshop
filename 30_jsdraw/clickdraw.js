999add1045d3bd7138a92f042ae0eb16cb8cdd76
iamchwill <wchen20@stuy.edu>
2022-02-15 07:51:07 -0500

    Finished Canvas!

--- 30_jsdraw/clickdraw.js
+++ 30_jsdraw/clickdraw.js
@@ -19,29 +19,44 @@ var toggleMode = function(e) {
 }
 
 var drawRect = function(e) {
-    var mouseX = c.offsetX;
-    var mouseY = c.offsetY;
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    ctx.beginPath()
+    ctx.rect(mouseX, mouseY, 100, 50)
+    ctx.fill();
+    ctx.stroke();
     console.log("mouseclick registered at ", mouseX, mouseY);
 }
 
 //var drawCircle = function(e) {
 var drawCircle = (e) => {
+    var mouseX = e.offsetX;
+    var mouseY = e.offsetY;
+    ctx.beginPath()
+    ctx.arc(mouseX, mouseY, 50, 0, 2 * Math.PI);
+    ctx.fill();
+    ctx.stroke();
     console.log("mouseclick registered at ", mouseX, mouseY);
-
 }
 
 //var draw = function(e) {
 var draw = (e) => {
-
+  if (mode == "rect") {
+    drawRect(e);
+  }
+  else {
+    drawCircle(e);
+  }
 }
 
 //var wipeCanvas = function() {
 var wipeCanvas = () => {
-    c.clearRect(0,0, c.clientWidth, c.clientHeight);
+    console.log("wiping?");
+    ctx.clearRect(0,0, c.width, c.height);
 }
 
 c.addEventListener("click", draw)
-var bToggler = document.
-bToggler. ;
-var clearB = ;
-clearB. ;
+var bToggler = document.getElementById("buttonToggle")
+bToggler.addEventListener("click", toggleMode);
+var clearB = document.getElementById("buttonClear");
+clearB.addEventListener("click", wipeCanvas);
